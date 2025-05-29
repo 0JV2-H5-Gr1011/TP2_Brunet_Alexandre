f8af718482246e81d6a628f17858f83d65937259
Walbio <AlexandreSoftware@proton.me>
2025-05-29 18:22:47 -0400

    Ajout Cooldown Degat Ennemi Suivi

--- Assets/Scripts/Plage/EnnemiSuivi.cs
+++ Assets/Scripts/Plage/EnnemiSuivi.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Ennemi : MonoBehaviour
+public class EnnemiSuivi : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform cible;
+    public NavMeshAgent agent;
+    public float delaiCollision = 2f;
+
+    Transform positionDepartJoueur;
+    float dernierContact = -999f;
+
     void Start()
     {
-        
+        if (agent == null)
+            agent = GetComponent<NavMeshAgent>();
+
+        if (cible != null)
+        {
+            GameObject vide = new GameObject("DepartJoueur");
+            vide.transform.position = cible.position;
+            positionDepartJoueur = vide.transform;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (cible != null)
+            agent.SetDestination(cible.position);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && Time.time - dernierContact >= delaiCollision)
+        {
+            GameObject[] points = GameObject.FindGameObjectsWithTag("Point");
+            if (points.Length > 0)
+            {
+                int index = Random.Range(0, points.Length);
+                Destroy(points[index]);
+            }
+
+            if (positionDepartJoueur != null)
+                other.transform.position = positionDepartJoueur.position;
+
+            dernierContact = Time.time;
+        }
     }
 }

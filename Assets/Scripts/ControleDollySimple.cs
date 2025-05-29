ea1ab5786d08e939efe15b102e64edea4f1037f0
Walbio <AlexandreSoftware@proton.me>
2025-05-29 04:16:18 -0400

    Fix Controle Dolly

--- Assets/Scripts/ControleDollySimple.cs
+++ Assets/Scripts/ControleDollySimple.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ControleDollySimple : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public MonoBehaviour entreeJoueur;
+    public Canvas[] interfacesUI;
+    public Volume volumeGlobal;
+    public Animator animDolly;
+    public string nomAnimation = "DollyMove";
+
+    public void DemarrerDolly()
     {
-        
+        entreeJoueur.enabled = false;
+        foreach (var ui in interfacesUI) ui.enabled = false;
+        volumeGlobal.enabled = true;
+        animDolly.Play(nomAnimation, 0, 0);
+        StartCoroutine(AttendreFin());
     }
 
-    // Update is called once per frame
-    void Update()
+    System.Collections.IEnumerator AttendreFin()
     {
-        
+        while (animDolly.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+            yield return null;
+
+        entreeJoueur.enabled = true;
+        foreach (var ui in interfacesUI) ui.enabled = true;
+        volumeGlobal.enabled = false;
     }
 }

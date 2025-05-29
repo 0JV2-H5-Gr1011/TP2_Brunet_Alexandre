cacc6e9307af06dcf6b8268054f5e677fbd41121
Walbio <AlexandreSoftware@proton.me>
2025-05-29 10:26:12 -0400

    Changement Musique

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -4,11 +4,15 @@ using UnityEngine.SceneManagement;
 public class ChangerScene : MonoBehaviour
 {
     [SerializeField] private string sceneACharger;
-    [SerializeField] private string sceneASupprimer;
+    [SerializeField] private GameObject sceneAMasquer;
 
     public void ChargerScene()
     {
-        SceneManager.LoadScene(sceneACharger, LoadSceneMode.Additive);  
-        SceneManager.UnloadSceneAsync(sceneASupprimer);
+        SceneManager.LoadScene(sceneACharger, LoadSceneMode.Additive);
+
+        if (sceneAMasquer != null)
+        {
+            sceneAMasquer.SetActive(false);
+        }
     }
 }
